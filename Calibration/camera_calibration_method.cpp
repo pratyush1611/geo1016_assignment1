5e7348e4241288b48d07b9c913fa2b79c025c499
Pratyush Kumar <51953929+pratyush1611@users.noreply.github.com>
2021-03-05 15:24:24 +0100

    update file latest from website, has few new lines

--- Calibration/camera_calibration_method.cpp
+++ Calibration/camera_calibration_method.cpp
@@ -34,8 +34,8 @@ using namespace easy3d;
  * TODO: Finish this function for calibrating a camera from the corresponding 3D-2D point pairs.
  *       You may define a few functions for some sub-tasks.
  *
- * @param points_3d   A array of 3D points.
- * @param points_2d   A array of 2D points.
+ * @param points_3d   An array of 3D points.
+ * @param points_2d   An array of 2D points.
  * @return True on success, otherwise false. On success, the camera parameters are returned by
  *           - fx and fy: the focal length (in our slides, we use 'alpha' and 'beta'),
  *           - cx and cy: the principal point (in our slides, we use 'u0' and 'v0'),
@@ -52,14 +52,12 @@ bool CameraCalibration::calibration(
         mat3& R,
         vec3& t)
 {
-    std::cout << "TODO: I am going to implement calibration() ..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "TODO: I am going to implement the calibration() function in the following file:" << std::endl
+              << "\t" << __FILE__ << std::endl;
+    std::cout << "TODO: After implementing the calibration() function, I will disable all unrelated output ...\n\n";
 
-    // TODO: check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points don't match)
-    if (points_3d.size < 6) 
-    {
-        std::cout "number of points is less than requirements. points must be more than or equal to 6" << std::endl; return false;
-    }
-        
+    // TODO: check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
 
     // TODO: construct the P matrix (so P * m = 0).
 
@@ -71,35 +69,37 @@ bool CameraCalibration::calibration(
 
     // TODO: extract extrinsic parameters from M.
 
-    // TODO: return true when testing your algorithm and in you final submission.
+    // TODO: uncomment the line below to return true when testing your algorithm and in you final submission.
     //return false;
 
+
+
+    // TODO: The following code is just an example showing you SVD decomposition, matrix inversion, and some related.
+    // TODO: Delete the code below (or change "#if 1" in the first line to "#if 0") in you final submission.
 #if 1
-    /**
-     * [Liangliang:] Camera calibration requires computing the SVD and inverse of matrices. The matrix classes (i.e.,
-     * mat3 and mat4) you've implemented in the first assignment only support matrices of a fixed-dimension. However,
-     * the input to the calibration problem can be arbitrary size (i.e., n>=6). Thus, in this assignment, I provide you
-     * with such an implementation (see matrix.h). I also wrote the following code just to show you how to
-     *   - use the dynamic 1D array data structure 'std::vector' from the standard C++ library; The points (both 3D and
-     *     2D are) stored in such arrays;
-     *   - use the template matrix class (which can have arbitrary size);
-     *   - compute the SVD of a matrix;
-     *   - compute the inverse of a matrix;
-     *   - compute the transpose of a matrix.
-     *   TODO: you should delete the following code in you final submission.
-     */
+    std::cout << "[Liangliang:] Camera calibration requires computing the SVD and inverse of matrices.\n"
+                 "\tIn this assignment, I provide you with a Matrix data structure for storing matrices of arbitrary\n"
+                 "\tsizes (see matrix.h). I also wrote the example code to show you how to:\n"
+                 "\t\t- use the dynamic 1D array data structure 'std::vector' from the standard C++ library;\n"
+                 "\t\t  The points (both 3D and 2D) are stored in such arrays;\n"
+                 "\t\t- use the template matrix class (which can have an arbitrary size);\n"
+                 "\t\t- compute the SVD of a matrix;\n"
+                 "\t\t- compute the inverse of a matrix;\n"
+                 "\t\t- compute the transpose of a matrix.\n"
+                 "\tThe following are just the output of these examples. You should delete ALL unrelated code and\n"
+                 "\tavoid unnecessary output in you final submission.\n\n";
 
     // This is a 1D array of 'double' values. Alternatively, you can use 'double mat[25]' but you cannot change it
     // length. With 'std::vector', you can do append/delete/insert elements, and much more. The 'std::vector' can store
     // not only 'double', but also any other types of objects. In case you may want to learn more about 'std::vector'
-    // https://en.cppreference.com/w/cpp/container/vector
-    std::vector<double> array = {1, 3, 3, 4, 7, 6, 2, 8, 2, 8, 3, 2, 4, 9, 1, 7, 3, 6, 1, 6, 2, 1, 5, 2, 3, 2, 7, 8, 1};
+    // check here: https://en.cppreference.com/w/cpp/container/vector
+    std::vector<double> array = {1, 3, 3, 4, 7, 6, 2, 8, 2, 8, 3, 2, 4, 9, 1, 7, 3, 23, 2, 3, 5, 2, 1, 5, 8, 9, 22};
     array.push_back(5); // append 5 to the array (so the size will increase by 1).
     array.insert(array.end(), 10, 3);  // append ten 3 (so the size will grow by 10).
 
-    // To access it values
+    // To access its values
     for (int i=0; i<array.size(); ++i)
-        std::cout << array[i] << " ";  // use 'array[i]' to access it i-th element.
+        std::cout << array[i] << " ";  // use 'array[i]' to access its i-th element.
     std::cout << std::endl;
 
     // Define an m-by-n double valued matrix.
@@ -111,6 +111,8 @@ bool CameraCalibration::calibration(
     Matrix<double> U(m, m, 0.0);   // initialized with 0s
     Matrix<double> S(m, n, 0.0);   // initialized with 0s
     Matrix<double> V(n, n, 0.0);   // initialized with 0s
+
+    // Compute the SVD decomposition of A
     svd_decompose(A, U, S, V);
 
     // Now let's check if the SVD result is correct
@@ -139,20 +141,4 @@ bool CameraCalibration::calibration(
     // TODO: delete the above code in you final submission (which are just examples).
 #endif
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
